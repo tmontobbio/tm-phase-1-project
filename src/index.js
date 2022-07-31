606bbb9ae4c4345011c393050a64991b7ad6cf50
Tyler <toygrrr@gmail.com>
2022-07-30 18:29:44 -0700

    Initial css ect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("testing");
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://dog.ceo/api/breed/labrador/random")
+    .then((res) => res.json())
+    .then((data) => data.forEach((labs) => generateDoggo(labs)));
+});
+
+// Ask for help to have a default image
+// Ask for help to make a generate dog button
+
+const rateButton = document
+  .querySelector("rating-form")
+  .addEventListener("submit", rateDoggo);
+const addButton = document
+  .querySelector("submit-dog")
+  .addEventListener("submit", addDoggo);
+
+function generateDoggo(labs) {
+  console.log(labs);
+}
+
+function rateDoggo() {}
+
+function addDoggo() {}
