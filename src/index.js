c49e86a2c704845be28b41221ee7af826eb39609
Tyler <toygrrr@gmail.com>
2022-08-01 16:54:47 -0700

    All functionality working

--- src/index.js
+++ src/index.js
@@ -1,54 +1,58 @@
-const likesForm = document.querySelector("#add-likes");
+const likesForm = document.querySelector("#add-rating");
+const submitBtn = document.querySelector("#submit-dog");
 const nextDoggoBtn = document.querySelector("#next-dog");
 const addVotes = document.querySelector("#more-likes");
 const p = document.querySelector("#like-value");
 const currentLikes = p.textContent;
-// Fetch dog URL's
+const formText = document.querySelector("#new-doggo");
+let container = document.querySelector("#doggo-img-container");
 
-document.addEventListener("DOMContentLoaded", function originalCall() {
+// Fetch dog URL's
+document.addEventListener("DOMContentLoaded", () => {
   fetch("https://dog.ceo/api/breed/labrador/images")
     .then((res) => res.json())
-    .then((data) => dataToArray(data));
-  nextDoggoBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    originalCall();
-  });
-  // need to pass this info to make it callable
-
-  // Push dog URL's into an empty array
-  function dataToArray(data) {
-    let dogArray = [];
-    dogArray.push(data);
-    randomDoggo(dogArray[0].message);
-  }
-
-  // Pass array to randomly select a key
-  function randomDoggo(array) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const randomDoggo = array[randomIndex];
-    imageGenerator(randomDoggo); // Need to access array values which are URL's
-  }
-
-  // Append random key into image container DIV
-  function imageGenerator(random) {
-    let container = document.querySelector("#doggo-img");
-    let newDoggo = (container.innerHTML = `<img src="${random}" />`);
-    let filler = document.querySelector("img");
-    filler.id = "doggo-filler";
-  }
+    .then((data) => {
+      randomDoggo(data.message);
+      nextDoggoBtn.addEventListener("click", () => {
+        p.textContent = "";
+        randomDoggo(data.message);
+      });
+    });
 });
 
-// Need to fix this and make counter work
-likesForm.addEventListener("submit", addLikes);
-
-function addLikes(e) {
-  e.preventDefault();
-  likes = parseInt(addVotes.value) + parseInt(currentLikes);
-  p.textContent = likes;
+// Pass object and randomly select one.  Also selects the default image when page is loaded.
+function randomDoggo(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const randomDoggo = array[randomIndex];
+  imageGenerator(randomDoggo);
 }
 
-//where do i put this
-// nextDoggoBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   originalCall();
-// });
+// Rating out of 10
+likesForm.addEventListener("submit", addLikes);
+function addLikes(e) {
+  e.preventDefault();
+  likes = Number(addVotes.value) + Number(currentLikes);
+  p.textContent = likes;
+  e.target.reset();
+}
+
+// Append random URL into image container DIV
+function imageGenerator(randomUrl) {
+  container.innerHTML = `<img src="${randomUrl}" />`;
+  let fillerImg = document.querySelector("img");
+  fillerImg.className = "doggo-filler-img";
+}
+
+// Dog 'submission' #2
+submitBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const url = formText.value;
+  e.target.reset();
+  if (url === "") {
+    alert("Please enter valid URL");
+  } else {
+    container.innerHTML = `<img src="${url}" />`;
+    let fillerImg = document.querySelector("img");
+    fillerImg.className = "doggo-filler-img";
+  }
+});
