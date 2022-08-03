29876505aed9fe8e1e8b3edc8572a9604ae0cf60
Tyler <toygrrr@gmail.com>
2022-08-03 16:04:48 -0700

    polished up functionality and favorites

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+// Global variables
 const likesForm = document.querySelector("#add-rating");
 const submitBtn = document.querySelector("#submit-dog");
 const nextDoggoBtn = document.querySelector("#next-dog");
 const addVotes = document.querySelector("#more-likes");
 const p = document.querySelector("#like-value");
 const currentLikes = p.textContent;
-const formText = document.querySelector("#new-doggo");
+const urlText = document.querySelector("#new-doggo");
+const rateText = document.querySelector("#more-likes");
+const favoriteContainer = document.querySelector("#favorite-doggos");
 let container = document.querySelector("#doggo-img-container");
 
 // Fetch dog URL's
@@ -34,19 +37,36 @@ function addLikes(e) {
   likes = Number(addVotes.value) + Number(currentLikes);
   p.textContent = likes;
   e.target.reset();
+  if (rateText.value > 10 && rateText.value < 10) {
+    alert("Please enter a valid rating. (HINT) Rating must be 10!");
+  } else {
+    p.textContent = likes;
+  }
 }
 
 // Append random URL into image container DIV
 function imageGenerator(randomUrl) {
-  container.innerHTML = `<img src="${randomUrl}" />`;
+  const displayImg = (container.innerHTML = `<img src="${randomUrl}" />`);
   let fillerImg = document.querySelector("img");
   fillerImg.className = "doggo-filler-img";
+  const favoriteBtn = document.querySelector("#favorite-button");
+  favoriteBtn.remove();
+  const newFavoriteBtn = document.createElement("button");
+  newFavoriteBtn.id = "favorite-button";
+  newFavoriteBtn.textContent = "Favorite ♥";
+  document.querySelector("#next-doggo-container").append(newFavoriteBtn);
+  newFavoriteBtn.addEventListener("click", (e) => {
+    const favoriteImg = document.createElement("img");
+    favoriteImg.className = "favorited-doggo-img";
+    favoriteContainer.append(favoriteImg);
+    favoriteImg.src = `${randomUrl}`;
+  });
 }
 
-// Dog 'submission' #2
+// 'Submit' new dog
 submitBtn.addEventListener("submit", (e) => {
   e.preventDefault();
-  const url = formText.value;
+  const url = urlText.value;
   e.target.reset();
   if (url === "") {
     alert("Please enter valid URL");
@@ -56,3 +76,6 @@ submitBtn.addEventListener("submit", (e) => {
     fillerImg.className = "doggo-filler-img";
   }
 });
+
+//push initial fetch data into a named array and iterate one by one instead of randomizer
+//every saved favorite dog gets put into a empty array, then we can use an iterator to reveal those urls under the favorited image
