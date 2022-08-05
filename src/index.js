de3dad17562436073b2bb34d6896f1ac5f5f72f6
Tyler <toygrrr@gmail.com>
2022-08-05 11:29:23 -0700

    final touches

--- src/index.js
+++ src/index.js
@@ -1,79 +1,113 @@
 // Global variables
-const likesForm = document.querySelector("#add-rating");
+const ratingForm = document.querySelector("#add-rating");
 const submitBtn = document.querySelector("#submit-dog");
 const nextDoggoBtn = document.querySelector("#next-dog");
-const addVotes = document.querySelector("#more-likes");
+let addRating = document.querySelector("#more-likes");
 const p = document.querySelector("#like-value");
-const currentLikes = p.textContent;
+const currentRating = p.textContent;
 const urlText = document.querySelector("#new-doggo");
 const rateText = document.querySelector("#more-likes");
 const favoriteContainer = document.querySelector("#favorite-doggos");
 let container = document.querySelector("#doggo-img-container");
-
+let dogs = [];
 // Fetch dog URL's
 document.addEventListener("DOMContentLoaded", () => {
   fetch("https://dog.ceo/api/breed/labrador/images")
     .then((res) => res.json())
     .then((data) => {
-      randomDoggo(data.message);
-      nextDoggoBtn.addEventListener("click", () => {
-        p.textContent = "";
-        randomDoggo(data.message);
+      dogs = data.message.map((url) => {
+        return { favorited: false, image: url };
       });
+      p.textContent = "0/10";
+      randomDoggo();
     });
 });
 
-// Pass object and randomly select one.  Also selects the default image when page is loaded.
-function randomDoggo(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  const randomDoggo = array[randomIndex];
+nextDoggoBtn.addEventListener("click", () => {
+  p.textContent = "0/10";
+  randomDoggo();
+});
+
+//Image randomizer
+function randomDoggo() {
+  const randomIndex = Math.floor(Math.random() * dogs.length);
+  const randomDoggo = dogs[randomIndex];
   imageGenerator(randomDoggo);
 }
-
 // Rating out of 10
-likesForm.addEventListener("submit", addLikes);
-function addLikes(e) {
+ratingForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  likes = Number(addVotes.value) + Number(currentLikes);
-  p.textContent = likes;
-  e.target.reset();
-  if (rateText.value > 10 && rateText.value < 10) {
+  if (addRating.value === "") {
+    alert("Please enter a valid rating. (HINT) Rating must be 10!");
+  } else if (parseInt(addRating.value) < "10") {
+    alert("Please enter a valid rating. (HINT) Rating must be 10!");
+  } else if (parseInt(addRating.value) > "10") {
     alert("Please enter a valid rating. (HINT) Rating must be 10!");
   } else {
-    p.textContent = likes;
+    p.textContent = addRating.value + "/10";
+    document.querySelector("#favorite-button").disabled = false;
   }
-}
+  e.target.reset();
+});
 
 // Append random URL into image container DIV
-function imageGenerator(randomUrl) {
-  const displayImg = (container.innerHTML = `<img src="${randomUrl}" />`);
+function imageGenerator(dog) {
+  const displayImg = (container.innerHTML = `<img src="${dog.image}" />`);
+  const favoriteBtn = document.querySelector("#favorite-button");
+  const newFavoriteBtn = document.createElement("button");
+  newFavoriteBtn.disabled = true;
   let fillerImg = document.querySelector("img");
   fillerImg.className = "doggo-filler-img";
-  const favoriteBtn = document.querySelector("#favorite-button");
   favoriteBtn.remove();
-  const newFavoriteBtn = document.createElement("button");
   newFavoriteBtn.id = "favorite-button";
   newFavoriteBtn.textContent = "Favorite ♥";
-  document.querySelector("#next-doggo-container").append(newFavoriteBtn);
+  document.querySelector("#doggo-button-container").append(newFavoriteBtn);
   newFavoriteBtn.addEventListener("click", (e) => {
-    const favoriteImg = document.createElement("img");
-    favoriteImg.className = "favorited-doggo-img";
-    favoriteContainer.append(favoriteImg);
-    favoriteImg.src = `${randomUrl}`;
+    dog.favorited = true;
+    displayFavorites(dog);
+    p.textContent = "0/10";
+  });
+}
+
+//filters the dog array and clears the favorite dogs section, and repopulates with favorited dogs
+function displayFavorites(dog) {
+  randomDoggo();
+  const favoriteDogs = dogs.filter((dog) => {
+    return dog.favorited;
+  });
+
+  favoriteContainer.innerHTML = "";
+  favoriteDogs.forEach((dog) => {
+    let dogCard = document.createElement("div");
+    dogCard.className = "dog-card";
+    let image = document.createElement("img");
+    image.src = dog.image;
+    image.className = "favorited-doggo-img";
+    favoriteContainer.append(dogCard);
+    dogCard.append(p.textContent);
+    dogCard.append(image);
   });
 }
 
 // 'Submit' new dog
 submitBtn.addEventListener("submit", (e) => {
   e.preventDefault();
+
   const url = urlText.value;
   e.target.reset();
   if (url === "") {
     alert("Please enter valid URL");
   } else {
-    container.innerHTML = `<img src="${url}" />`;
-    let fillerImg = document.querySelector("img");
-    fillerImg.className = "doggo-filler-img";
+    let dogCard = document.createElement("div");
+    dogCard.class = "dog-card";
+    let image = document.createElement("img");
+    image.src = url;
+    image.className = "favorited-doggo-img";
+    dogCard.append(image);
+    p.textContent = "10/10";
+    dogCard.append(p.textContent);
+    favoriteContainer.append(dogCard);
+    //dogCard.append(10 + "/10");
   }
 });
 
